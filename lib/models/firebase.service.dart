96d247490d5f85494343645079a4bdfdb4c5191e
VAIBHAV HARIRAMANI <vaibhav.hariramani01@gmail.com>
2024-02-07 06:37:27 +0000

    changes

--- lib/models/firebase.service.dart
+++ lib/models/firebase.service.dart
@@ -1,8 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
-import 'package:vdsadmin/category/category.dart';
-import 'package:vdsadmin/models/barcodescanner.dart';
 
 import 'package:fluttertoast/fluttertoast.dart';
 
