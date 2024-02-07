05edba9b54bc67e310febd8b56b1b268e0c2405c
vhariramani <vhariramani@avaya.com>
2024-02-07 15:11:30 +0000

    changes

--- lib/app/modules/billing/views/billing_view.dart
+++ lib/app/modules/billing/views/billing_view.dart
@@ -1,18 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_listener/flutter_barcode_listener.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_dashboard/flutter_dashboard.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import 'package:get/get.dart';
+import 'package:vdsadmin/app/modules/billing/views/widgets/createItem.dart';
 
 import '../../../../constants/constants.dart';
+import '../../../../models/Product.dart';
+import '../../../../models/invoice.dart';
 import '../../../../themes/app_theme.dart';
 import '../../../widgets/components/common_card.dart';
 import '../../../widgets/utils/padding_wrapper.dart';
 import '../../deletion_status/views/deletion_status_view.dart';
 import '../../home/views/home_view.dart';
+import '../../orders/views/order_details.dart';
 import '../controllers/billing_controller.dart';
 
 class BillingView extends GetResponsiveView<BillingController> {
   BillingView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     screen.context = context;
@@ -51,6 +62,86 @@ class BillingView extends GetResponsiveView<BillingController> {
               ),
             ),
           ),
+          SliverVisibility(
+            visible: !deletionStatusController.isVisible.value,
+            sliver: PaddingWrapper(
+                isSliverItem: true,
+                horizontalPadding: 20,
+                topPadding: 20,
+                child: FlutterDashboardListView(
+                  slivers: [
+                    Expanded(
+                      // height: MediaQuery.of(context).size.height * 0.3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CachedNetworkImage(
+                              imageUrl:
+                                  'https://firebasestorage.googleapis.com/v0/b/atus-kart.appspot.com/o/static%2Fbasket.png?alt=media&token=4ca7a331-90d3-4ce0-8113-0226e577085e',
+                              width: 120,
+                              height: 120),
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8, top: 12),
+                            child: Text('No items in your cart!',
+                                style: TextStyle(color: Colors.grey)),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 60, right: 60),
+                            child: Text(
+                              "We are looking to provide our services for you",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          ), // ListView(
+          //   // shrinkWrap: true,
+          //   physics: BouncingScrollPhysics(),
+          //   children: [
+
+          // ListView.builder(
+          //   // shrinkWrap: true,
+          //   scrollDirection: Axis.vertical,
+          //   physics: const BouncingScrollPhysics(),
+          //   itemBuilder: buildProductItem,
+          //   itemCount: controller.products.length,
+          // ),
+          // Center(
+          //   // Add visiblity detector to handle barcode
+          //   // values only when widget is visible
+          //   child: VisibilityDetector(
+          //     onVisibilityChanged: (VisibilityInfo info) {
+          //       controller.visible = info.visibleFraction > 0;
+          //     },
+          //     key: Key('visible-detector-key'),
+          //     child: BarcodeKeyboardListener(
+          //       bufferDuration: Duration(milliseconds: 200),
+          //       onBarcodeScanned: (barcode) {
+          //         controller.productfetcher((barcode));
+          //       },
+          //       // child: SizedBox(
+          //       //   height: 1,
+          //       // ),
+          //       child: Column(
+          //         children: <Widget>[
+          //           Text(
+          //             controller.barcode == null
+          //                 ? 'Waiting for new BARCODE'
+          //                 : 'BARCODE: ${controller.barcode}',
+          //             style: Theme.of(context).textTheme.headline5,
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          //   ],
+          // ),
         ],
       ),
     );
